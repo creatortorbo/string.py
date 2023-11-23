8155a09af6c8d03aca7f8e6f4675f514dc21bd2f
bablu kumar pandey <141337235+creatortorbo@users.noreply.github.com>
2023-11-23 05:57:33 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,18 @@
+name = "bablu"
+friend = "Rohan"
+anotherfriend = "vishal"
+apple = '''he said 
+my name is j
+bablu
+love you 
+my dream girls j
+'''
+# print(name)
+# print("hello,"+ name)
+# # print(apple)
+print (name[0])
+print (name[1])
+print (name[2])
+print (name[3])
+print (name[4])
+print (name[5])# it shows an error 
