b3e7300918f6ff75f88826fe27be3a7c72f891e0
bablu kumar pandey <141337235+creatortorbo@users.noreply.github.com>
2023-11-23 05:58:30 +0530

    Add files via upload

---
+++ pintingAllString.py
@@ -0,0 +1,11 @@
+
+apple = '''he said 
+my name is j
+bablu
+love you 
+my dream girls j
+'''
+print("lets use a for loops\n")
+for character  in apple :
+    print(character)                           
+                              
