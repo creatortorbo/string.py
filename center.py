cc08c688922eafc2c5f2ec9c37b251f403aff9da
bablu kumar pandey <141337235+creatortorbo@users.noreply.github.com>
2023-11-23 05:58:02 +0530

    Add files via upload

---
+++ center.py
@@ -0,0 +1,16 @@
+str = "Welcome to the console!!!"
+print(len(str))
+print(str.center(50))
+print(len(str.center(50)))
+# count the string 
+a = "bablu kumar pandey bablu"
+print(a.count("bablu"))
+print(str.endswith("!!!"))
+
+
+str = "welcome to the console !!!"
+print(str.endswith("to",4,10))
+
+str = "He's name is Dan . He is an honest man ."
+print(str.find ("is")) # index method 
+print(str.find("0000"))#-1
