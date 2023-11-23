10304cfb17a538e0374f6fb94f7feec6ef78239f
bablu kumar pandey <141337235+creatortorbo@users.noreply.github.com>
2023-11-23 05:59:47 +0530

    Add files via upload

---
+++ StringSlicing.py
@@ -0,0 +1,12 @@
+
+name = "bablu,pandey" 
+print(len(name))#12
+print(name[0:7])#bablu,p
+print(name[:7])#bablu,p
+print(name[1:7])#ablu,p
+print(name[:])#bablu,pandey
+print(name[:-3])#bablu,pan
+print(name[:-8])#babl
+print(name[0:len(name)-3])
+print(name[-1:len(name)-3])
+
