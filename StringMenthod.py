88435ed41145bcc5b809d08ce07d9236c990410b
bablu kumar pandey <141337235+creatortorbo@users.noreply.github.com>
2023-11-23 05:59:17 +0530

    Add files via upload

---
+++ StringMenthod.py
@@ -0,0 +1,13 @@
+# string are immutable
+# a = "bablu"
+# a = "BABLU"
+a = "!!!!!bablu! !!!!!!! bablu"
+# print(len(a))
+# # print(a.upper()) 
+# print(a.lower()) 
+print(a.rstrip("!"))
+print(a.replace("bablu","pandey"))
+print(a.split(" "))
+blogHeading = "introduction tO js "
+print(blogHeading.capitalize())
+
